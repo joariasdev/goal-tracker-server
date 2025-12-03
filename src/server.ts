dae3c84c02b155e1b236f26d48370b2ab6e559b6
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-12-03 15:02:41 -0400

    Add pg to control database connections

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import config from './config/config';
 import express from 'express';
+import db from './config/db';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello, world!")
-})
+  res.send('Hello, world!');
+});
 
-app.listen(config.port, () => console.log(`Server is running on port: ${config.port}`))
+app.listen(config.port, () =>
+  console.log(`Server is running on port: ${config.port}`),
+);
