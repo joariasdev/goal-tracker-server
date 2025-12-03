35f671d4f594fa1282a8fe9fc3178d1f783a1f0b
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-12-03 15:51:45 -0400

    Create POST /goals endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import config from './config/config';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import db from './config/db';
 
 const app = express();
 
+app.disable('x-powered-by');
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.post('/goals', async (req: Request, res: Response) => {
+  const { title } = req.body;
+  const result = await db.query(
+    'INSERT INTO goals (title) VALUES($1) RETURNING *',
+    [title],
+  );
+  res.status(201).send(result.rows);
 });
 
 app.listen(config.port, () =>
