a76299d1ed9538a3ae3c895144ced22bfa02f43e
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-12-03 16:44:06 -0400

    Add types to POST /goals endpoint

---
+++ src/models/CreateGoalRequestBody.ts
@@ -0,0 +1,3 @@
+export default interface CreateGoalRequestBody {
+  title: string;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import config from './config/config';
 import express, { Request, Response } from 'express';
 import db from './config/db';
+import CreateGoalRequestBody from './models/CreateGoalRequestBody';
+import Goal from './models/Goal';
 
 const app = express();
 
@@ -12,14 +14,18 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.post('/goals', async (req: Request, res: Response) => {
-  const { title } = req.body;
-  const result = await db.query(
-    'INSERT INTO goals (title) VALUES($1) RETURNING *',
-    [title],
-  );
-  res.status(201).send(result.rows);
-});
+app.post(
+  '/goals',
+  async (req: Request<any, any, CreateGoalRequestBody>, res: Response) => {
+    const { title } = req.body;
+
+    const result = await db.query<Goal>(
+      'INSERT INTO goals (title) VALUES($1) RETURNING *',
+      [title],
+    );
+    res.status(201).send(result.rows);
+  },
+);
 
 app.listen(config.port, () =>
   console.log(`Server is running on port: ${config.port}`),
